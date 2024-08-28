b187ce778b8db69bd70734726c510c3139fc27f7
milanogrizovic <milan.ogrizovic@productdock.com>
2024-08-28 22:27:52 +0200

    fix controller

--- src/main/java/com/example/elkdemo/TestDocumentController.java
+++ src/main/java/com/example/elkdemo/TestDocumentController.java
@@ -1,9 +1,9 @@
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class TestDocumentController {
 
     @Autowired
